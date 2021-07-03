508cac41241ead18f4a88fd054f81e954550e3cd
Vlad Popov <scientific1atheism@gmail.com>
2021-07-03 14:27:10 +0300

    add load more btn js

--- js/mainScripts.js
+++ js/mainScripts.js
@@ -1,11 +1,23 @@
 const loadMoreBtn = document.getElementById("loadMoreButton");
-
 const articles = document.getElementById("articlesList");
-console.log(articles)
-for (let i = 0; i < 2; i++) {
-  console.log(articles.children[i].tagName);
+let displayedArticles = 0
+
+displayArticles()
+
+function displayArticles() {
+  if (displayedArticles < articles.children.length) {
+    for (let i = displayedArticles; i < displayedArticles + 2; i++) {
+      articles.children[i].style.display="block"
+    }
+  
+    displayedArticles += 2
+  }
+
+  if (displayedArticles === articles.children.length) {
+    loadMoreBtn.style.display = "none"
+  }
 }
 
 if (loadMoreBtn) {
-    loadMoreBtn.addEventListener("click", () => console.log("Clicked"));
+    loadMoreBtn.addEventListener("click", () => displayArticles());
 }
